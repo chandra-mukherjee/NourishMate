5a519b4273dfa6964b382134300cb6769347768c
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-09-13 21:59:24 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,4 @@
-// Connecting react application to html file
 
-//  axios: API requests
-// moment: time and date lib
-// react - file - base64: converting images
-// redux, redux - thunk: asynchorous function using redux
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -18,7 +13,6 @@ import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-// connect to div of Id of root
 ReactDOM.render(
     <Provider store={store}>
         <App />
