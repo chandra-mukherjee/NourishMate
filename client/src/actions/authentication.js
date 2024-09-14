0ed08bfafe97005355081678bdda017a28620e9f
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-09-14 23:25:44 +0530

    Update authentication.js

--- client/src/actions/authentication.js
+++ client/src/actions/authentication.js
@@ -1,22 +1,22 @@
 import * as api from '../api/index.js';
 import { AUTH } from '../constants/constantTypes.js';
 import { setSnackBar } from './snackBar.js';
-// CREATE ACTION CREATORS
+
 
 // Sign In action
 export const signin = (formData, history) => async (dispatch) => {
     try {
         const { data } = await api.signIn(formData);
-        // dispatch AUTH type action with data as payload
+       
         dispatch({ type: AUTH, payload: data });
 
         const user = JSON.parse(localStorage.getItem('userProfile'));
 
         if (user?.userInfo?.role === "USER") {
-            // push back from current path to /dashboard
+          
             history.push('/dashboard');
         } else {
-            // push back from current path to /admin
+           
             history.push('/admin');
         }
 
@@ -29,15 +29,15 @@ export const signin = (formData, history) => async (dispatch) => {
     }
 };
 
-// Sign Up action
+
 export const signup = (formData, history) => async (dispatch) => {
     try {
         const { data } = await api.signUp(formData);
 
         dispatch({ type: AUTH, payload: data });
-        // push back from current path to /authentication
+       
         history.push('/authentication');
-        // console.log(data);
+       
         dispatch(setSnackBar(true, "success", "SUCCESSFULLY SIGNED UP"));
     } catch (error) {
         console.log(error.message);
