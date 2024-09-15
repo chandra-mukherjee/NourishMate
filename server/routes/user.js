1eebd4462d29788566849caa109911eee213bcbe
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-09-15 17:08:33 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,9 +3,4 @@ const router = express.Router();
 
 import { getUsers, deleteUser, signin, signup } from "../controllers/user.js";
 
-router.get("/user", getUsers);
-router.post("/user/signin", signin);
-router.post("/user/signup", signup);
-router.delete('/user/:id', deleteUser);
-
 export default router;
