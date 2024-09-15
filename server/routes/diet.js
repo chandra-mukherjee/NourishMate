aa3dfa0b6e481f12c2fcb16e8a219f82a2b4609f
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-09-15 17:07:21 +0530

    Update diet.js

--- server/routes/diet.js
+++ server/routes/diet.js
@@ -1,14 +1,3 @@
 import express from "express";
 import authentication from '../middleware/authentication.js';
 
-import { getDietPost, createDietPost, updateDietPost, likeDietPost, deleteDietPost } from '../controllers/diet.js';
-const router = express.Router();
-
-router.get('/diet', getDietPost);
-router.post('/diet', authentication, createDietPost);
-router.patch('/diet/:id', authentication, updateDietPost);
-router.patch('/diet/:id/likeDietPost', authentication, likeDietPost);
-router.delete('/diet/:id', authentication, deleteDietPost);
-
-
-export default router;
