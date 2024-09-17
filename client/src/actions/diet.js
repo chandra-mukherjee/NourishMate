edb00df43df6e363ac17a60cf7d6822f81edfc49
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-09-18 02:37:05 +0530

    Update diet.js

--- client/src/actions/diet.js
+++ client/src/actions/diet.js
@@ -1,19 +1,19 @@
 import * as api from '../api/index.js';
 import { FETCH1, CREATE1, UPDATE1, DELETE1, LIKE1 } from '../constants/constantTypes.js';
 import { setSnackBar } from './snackBar.js';
-// get Diet Posts
+
 export const getDietPost = () => async (dispatch) => {
     try {
         const { data } = await api.fetchDietPost();
         console.log(data)
-        // dispatch action  to get data
+      
         dispatch({ type: FETCH1, payload: data });
     } catch (error) {
         console.log(error.message);
     }
 };
 
-// create Diet Post
+s
 export const createDietPost = (dietPost) => async (dispatch) => {
     try {
         const { data } = await api.createDietPost(dietPost);
@@ -25,7 +25,7 @@ export const createDietPost = (dietPost) => async (dispatch) => {
     }
 };
 
-// update Diet Post
+
 export const updateDietPost = (id, dietPost) => async (dispatch) => {
     try {
         const { data } = await api.updateDietPost(id, dietPost);
@@ -37,7 +37,7 @@ export const updateDietPost = (id, dietPost) => async (dispatch) => {
     }
 };
 
-// like Diet Post
+
 export const likeDietPost = (id) => async (dispatch) => {
 
     try {
@@ -49,7 +49,7 @@ export const likeDietPost = (id) => async (dispatch) => {
     }
 };
 
-// delete Diet Post
+
 export const deleteDietPost = (id) => async (dispatch) => {
     try {
         await api.deleteDietPost(id);
