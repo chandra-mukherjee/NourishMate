6d57a188910cc4d0073bc144c37a69cb1facec49
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-09-17 18:58:19 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,14 @@
-import express from "express";
+import express from 'express';
+
+import { getHealthDetails, createHealthDetail, updateHealthDetail, deleteHealthDetail } from '../controllers/healthDetail.js';
+
+import authentication from '../middleware/authentication.js';
+
 const router = express.Router();
 
-import { getUsers, deleteUser, signin, signup } from "../controllers/user.js";
+router.get('/HD', getHealthDetails);
+router.post('/HD', authentication, createHealthDetail);
+router.patch('/HD/:id', authentication, updateHealthDetail);
+router.delete('/HD/:id', authentication, deleteHealthDetail);
 
 export default router;
