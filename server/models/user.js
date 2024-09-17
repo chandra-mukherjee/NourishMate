4b7a2f70a72aeb57a1e6ea1c4c34d01d8d4ea3d8
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-09-17 19:04:04 +0530

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,11 @@
-import mongoose from 'mongoose';
+import express from "express";
+const router = express.Router();
 
+import { getUsers, deleteUser, signin, signup } from "../controllers/user.js";
 
+router.get("/user", getUsers);
+router.post("/user/signin", signin);
+router.post("/user/signup", signup);
+router.delete('/user/:id', deleteUser);
 
-export default mongoose.model("User", userSchema);
-
+export default router;
