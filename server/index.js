cecf92e9509c282b05a281ff94eaee63ecdf1c5f
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-09-17 19:04:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
-
+/* 
+    body-parser: enable send requests
+    cors: enable cors orgin requests
+    express: routing epxress framework
+    mongoose: creating models / connect mongoDB
+    nodemon: auto reset
+*/
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -17,19 +23,19 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-
+// HD Router
 app.use('/', healthDetailRouter);
-
+// user Router
 app.use('/', userRouter);
-
-app.use('/', dietRouter);   
+// diet Router
+app.use('/', dietRouter);   // localhost:5000/diet/diet
 
 app.get('/', (req, res) => {
     res.send("Hello to API");
 }); 
 
-
-
+// connect to mongoDB 
+// const CONNECTION_URL = 'mongodb+srv://Liam:Liam1809@cluster0.ifm61.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 4000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(
@@ -47,4 +53,4 @@ app.listen(5000,()=>{
     console.log("Server is running on port: 5000")
 })
 
-mongoose.set('useFindAndModify', false); 
+mongoose.set('useFindAndModify', false); //  no warnings in the console
