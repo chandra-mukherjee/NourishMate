038d273e8a794b62c1f4c3d92b0f4545125f320e
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-09-25 21:56:37 +0530

    Update user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -94,8 +94,6 @@ export const signup = async (req, res) => {
         // create user token
         const token = jwt.sign({ email: newUser.email, name: newUser.name, id: newUser._id }, '@user', { expiresIn: '1h' });
         // console.log("signup token");
-        
-      
         res.status(200).json({ userInfo: newUser, token });
 
     } catch (error) {
