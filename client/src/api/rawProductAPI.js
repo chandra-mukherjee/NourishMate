4d5221af34b3b7669808396c95132ccad5b43657
Chandra Mukherjee <116421612+chandra-mukherjee@users.noreply.github.com>
2024-11-03 00:24:30 +0530

    Update rawProductAPI.js

--- client/src/api/rawProductAPI.js
+++ client/src/api/rawProductAPI.js
@@ -8,14 +8,3 @@ export const API = axios.create({
     }
 });
 
-// header1
-// headers: {
-//     'x-app-id': 'f7b33457',
-//     'x-app-key': '0c4bce30ca0088e1c732829e9b018c48'
-// }
-
-// header2
-// headers: {
-//     'x-app-id': '7b14ebd4',
-//     'x-app-key': 'f27ea0520b4309e18a209cf9aa440e42'
-// }
